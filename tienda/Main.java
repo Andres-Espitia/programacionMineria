3785e6a7925c9eff25b58ed9735f7fe174fd5731
unknown <andrefespitia2001@gmail.com>
2023-02-18 10:42:45 -0500

    primer commit

---
+++ tienda/Main.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+        do{
+
+
+        }while(true)
+        System.out.println("Catalogo tienda TEC");
+        System.out.println("1. Mouse -- $45000");
+        System.out.println("2. Teclados -- $60000");
+        System.out.println("3. Monitores -- $950000");
+        System.out.println("4. Ram -- $125000");
+        System.out.println("5. Discos Duros -- $280000");
+        System.out.println("6. Generar Factura");
+
+        int producto = 0;
+        System.out.println("Ingrese el numero del producto: ");
+        producto = teclado.nextInt();
+
+        int cantidad = 0;
+        System.out.println("Ingrese el numero del producto: ");
+        cantidad = teclado.nextInt();
+
+        ;
+    }
+}
